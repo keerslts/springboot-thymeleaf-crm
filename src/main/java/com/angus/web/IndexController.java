69d37617e1f3d23877760a4714d9b5f2cfef0727
c5288914 <zelong.he@sap.com>
2019-09-10 18:01:12 +0800

    账户界面跳转完成

--- src/main/java/com/angus/web/IndexController.java
+++ src/main/java/com/angus/web/IndexController.java
@@ -17,9 +17,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.security.auth.login.LoginContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.net.Inet4Address;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -28,6 +30,7 @@ public class IndexController {
 
     private static final String INDEX = "index";
     private static final String LOGIN = "login";
+    private static final String ACCOUNT = "account";
     @Autowired
     private MusicInfoService musicInfoService;
     @Autowired
@@ -52,13 +55,12 @@ public class IndexController {
      * 判断登陆信息，并将登陆用户信息存入session中
      * 该方法中的user为前台填入的信息
      * @param user
-     * @param rs
      * @param request
      * @return
      */
     @RequestMapping(value = "/userLogin",method = RequestMethod.POST)
     public String getLoginResult (@ModelAttribute("user") @Validated User user,
-                                  @ModelAttribute("message") Message message,
+//                                  @ModelAttribute("message") Message message,
                                   HttpServletRequest request)
     {
         httpSession = request.getSession();
@@ -67,14 +69,15 @@ public class IndexController {
         String passWord = user.getPassWord();
 
         List<User> userResults = null;
-        userResults = userService.getUserByNameAndPassword(user);
+//        userResults = userService.getUserByNameAndPassword(user);
         //没有查到登录用户
         if(userResults==null||userResults.size()==0){
             httpSession.setAttribute("wrongMessage","未查询到当前用户！");
-            message.setWrongMessage("未查询到当前用户！");
+//            message.setWrongMessage("未查询到当前用户！");
             return LOGIN;
         }
 
+        httpSession.setAttribute("wrongMessage","");
         //查询到登陆用户，默认只取第一个用户（不允许同名同密码用户）
         User currentUser = userResults.get(0);
 
@@ -83,22 +86,44 @@ public class IndexController {
         httpSession.setAttribute("userName",user.getUserName());
 
         //验证成功，我们可以返回到自己想去的界面了，我这个地方直接返回到添加的界面
-        return "index";
-    }
-
-    @RequestMapping("/index")
-    public String getIndex(HttpServletRequest request, @RequestParam(value = "name", defaultValue = "springboot-thymeleaf") MusicInfo musicInfo) {
         return INDEX;
     }
 
-    @RequestMapping("/account")
+    @RequestMapping("/index")
+    public String getIndex(HttpServletRequest request) {
+        if(!userSessionCheck(request)){
+            //如果session信息中没有用户信息
+            return LOGIN;
+        }
+        return INDEX;
+    }
+    @RequestMapping("/editUserInfo")
+    public String editUserInfo(HttpServletRequest request,Integer id,User user) {
+        httpSession.setAttribute("wrongMessage","");
+        if(!userSessionCheck(request)){
+            //如果session信息中没有用户信息
+            return LOGIN;
+        }
+        return INDEX;
+    }
+
+    @RequestMapping("/accountShow")
     public String getAccount(ModelMap map){
 
-        MusicInfo person = new MusicInfo();
-        person.setId(1);
-        person.setMusicName("zs");
+        List<User> userList = new ArrayList<User>();
+        User user = new User();
+        user.setId(1);
+        user.setUserName("1");
+        user.setRightLevel(1);
+        userList.add(user);
+        User user1 = new User();
+        user1.setId(2);
+        user1.setUserName("2");
+        user1.setRightLevel(2);
+        userList.add(user1);
 
-        map.put("person", person);
+
+        map.put("userList", userList);
 
         return "account";
     }
@@ -111,4 +136,16 @@ public class IndexController {
         return musicInfoList;
     }
 
+
+    public boolean userSessionCheck(HttpServletRequest request){
+
+        boolean flag = true;
+        httpSession = request.getSession();
+        String userName = (String)httpSession.getAttribute("userName");
+        if(null==userName){
+            flag = false;
+        }
+
+        return flag;
+    }
 }
